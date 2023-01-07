9c3c0968ce2d01cd73aa36700ad4207a732082de
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-01-07 02:57:15 -0500

    starting to learn python beginning with variables and conditions

---
+++ Python/conditions.py
@@ -0,0 +1,5 @@
+n = input("Number: ")
+
+# indentation is required this is how the code knows what is inside the if statement and what is outside
+if n > 0:
+    print("n is positive")
---
+++ Python/name.py
@@ -0,0 +1,8 @@
+# Prompts the user for input
+name = input("Name: ")
+
+# concatenation
+print("Hello, " + name)
+
+# formatted strings
+print(f"Hello: , {name}")
